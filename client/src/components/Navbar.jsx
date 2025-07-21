f78626538f5292089b5c4451215ab28353046d43
Veer Pardeshi <139315910+8767059947@users.noreply.github.com>
2025-07-21 14:28:41 +0530

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -33,7 +33,8 @@ function Navbar() {
       ) : (
         <button
           onClick={openSignIn}
-          className="flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-10 py-2.5"
+          className="flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-6 py-2 sm:px-10 sm:py-2.5"
+
         >
           Get started <ArrowRight className="w-4 h-4" />
         </button>
